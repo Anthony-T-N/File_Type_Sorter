11a2bae7410b29eb493d9e25d34a6d9db67e4fc6
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-10-08 13:05:31 +1030

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,13 +6,14 @@ using System.Collections.Generic;
  * 1) Select specific folder  
  * 2) Read through and extract all extensions of files of the folder and add to a list.
  * 3) Create new folders based on said extensions.
- * 4) Cut/Move files to allocated folders based on extensions.
+ * 4) Move files to allocated folders based on extensions.
  */
 
 namespace File_Type_Sorter
 {
     class Program
     {
+        private List<string> extension_list = new List<string>();
         static void Main(string[] args)
         {
             Program main_program = new Program();
@@ -25,21 +26,37 @@ namespace File_Type_Sorter
             // Get the current directory.
             string[] files = Directory.GetFiles(System.AppDomain.CurrentDomain.BaseDirectory);
             List<string> file_list = new List<string>();
-            List<string> extension_list = new List<string>();
             foreach (string file in files)
             {
                 string filename = Path.GetFileName(file);
                 Console.WriteLine(filename);
                 file_list.Add(filename);
                 string extension = filename.Substring(filename.LastIndexOf("."), filename.Length - filename.LastIndexOf("."));
-                extension_list.Add(extension);
+                if (!extension_list.Contains(extension))
+                {
+                    extension_list.Add(extension);
+                }
                 Console.WriteLine(extension);
             }
         }
         public void create_ext_folders()
         {
-            string pathString = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "SubFolder");
-            System.IO.Directory.CreateDirectory(pathString);
+            string current_path_string = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Sorted_Extension_Folder");
+            if (!System.IO.Directory.Exists(current_path_string))
+            {
+                System.IO.Directory.CreateDirectory(current_path_string);
+                Console.WriteLine("HELLO");
+            }
+            else
+            {
+                Console.WriteLine("File \"{0}\" already exists.", current_path_string);
+                return;
+            }
+            for (int i = 0; i <= extension_list.Count - 1; i++)
+            {
+                System.Console.WriteLine(System.IO.Path.Combine(current_path_string, extension_list[i]));
+                System.IO.Directory.CreateDirectory(System.IO.Path.Combine(current_path_string, extension_list[i]));
+            }
             Console.WriteLine("DONE");
         }
     }
