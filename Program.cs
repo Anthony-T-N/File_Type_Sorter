89f3f524f7cf7166b5c8bac7db3704230219dbcb
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-10-09 13:34:20 +1030

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,23 +14,27 @@ namespace File_Type_Sorter
     class Program
     {
         private List<string> extension_list = new List<string>();
+        private List<string> file_path_list = new List<string>();
+        //string target_location = System.AppDomain.CurrentDomain.BaseDirectory;
+        string target_location = @"C:\Users\Anthony\source\repos\File_Type_Sorter\bin\Debug\netcoreapp3.1\Target_Test_Folder\";
         static void Main(string[] args)
         {
             Program main_program = new Program();
             main_program.read_dir_files();
             main_program.create_ext_folders();
+            main_program.move_files();
      
         }
         public void read_dir_files()
         {
             // Get the current directory.
-            string[] files = Directory.GetFiles(System.AppDomain.CurrentDomain.BaseDirectory);
-            List<string> file_list = new List<string>();
+            string[] files = Directory.GetFiles(target_location);
+            
             foreach (string file in files)
             {
+                file_path_list.Add(file);
                 string filename = Path.GetFileName(file);
                 Console.WriteLine(filename);
-                file_list.Add(filename);
                 string extension = filename.Substring(filename.LastIndexOf("."), filename.Length - filename.LastIndexOf("."));
                 if (!extension_list.Contains(extension))
                 {
@@ -41,15 +45,15 @@ namespace File_Type_Sorter
         }
         public void create_ext_folders()
         {
-            string current_path_string = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Sorted_Extension_Folder");
+            string current_path_string = System.IO.Path.Combine(target_location, "Sorted_Extension_Folder");
             if (!System.IO.Directory.Exists(current_path_string))
             {
                 System.IO.Directory.CreateDirectory(current_path_string);
-                Console.WriteLine("HELLO");
+                Console.WriteLine("[+] Creating \"{0}\"", current_path_string);
             }
             else
             {
-                Console.WriteLine("File \"{0}\" already exists.", current_path_string);
+                Console.WriteLine("[-] File \"{0}\" already exists.", current_path_string);
                 return;
             }
             for (int i = 0; i <= extension_list.Count - 1; i++)
@@ -57,7 +61,18 @@ namespace File_Type_Sorter
                 System.Console.WriteLine(System.IO.Path.Combine(current_path_string, extension_list[i]));
                 System.IO.Directory.CreateDirectory(System.IO.Path.Combine(current_path_string, extension_list[i]));
             }
-            Console.WriteLine("DONE");
+        }
+        public void move_files()
+        {
+            for (int i = 0; i <= file_path_list.Count - 1; i++)
+            {
+                string filename = Path.GetFileName(file_path_list[i]);
+                Console.WriteLine(filename);
+                string extension = filename.Substring(filename.LastIndexOf("."), filename.Length - filename.LastIndexOf("."));
+                Console.WriteLine(extension);
+                File.Move(file_path_list[i], target_location + @"Sorted_Extension_Folder\" + extension + @"\" + filename);
+                Console.WriteLine("[+] {0} was moved to {1}.", file_path_list[i], target_location + @"Sorted_Extension_Folder\" + extension);
+            }
         }
     }
 }
